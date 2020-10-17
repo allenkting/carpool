7c7211b5c24b1a4ecab219a26876ef2bd3f08b22
zile151 <andyzile.zile@gmail.com>
2020-10-17 13:46:24 -0500

    html/css looking good

--- carpool.js
+++ carpool.js
@@ -1,18 +1,22 @@
 'use strict';
 // Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
+let script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDC9vuN0XPq_N-glaLd3ppqV2Y9o8E1m5M&callback=initMap';
 script.defer = true;
 
 let map;
+let form = document.getElementById("myForm");
+
 // Attach your callback function to the `window` object
 window.initMap = function() {
   // add the map 
   map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
+  center: {lat: 30.2849 , lng: -97.7341},
   zoom: 8
 	});
 };
 
+// form
 // Append the 'script' element to 'head'
 document.head.appendChild(script);
+
